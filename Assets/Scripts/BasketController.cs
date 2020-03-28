356d8db5eca64a0282450f0354a0069e6f6cec92
tomoya-takubo <tkb1003@gmail.com>
2020-03-29 00:53:24 +0900

    202003290051　コミット
    アイテム（リンゴ/爆弾）落下実装、かご移動実装
    Next　⇒　アイテムキャッチ実装

---
+++ Assets/Scripts/BasketController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public float dropSpeed = -0.03f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //　落下
+        transform.Translate(0, this.dropSpeed, 0);
+
+        //　一定の高さまで落ちたら消滅
+        if(transform.position.y < -1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
